edea8ef1845e213e47d56dd14c9659a40905c568
joaoluizcorreademello <joaoluiz.index@outlook.com>
2025-11-03 04:32:45 -0300

    Data esta funcionado corretamente, cargo pesquisa por qualquer luga que tenha o texto inserido, recentes transforma em timestamp e procurar vagas com data de publicacao maiores que o recentes, começando a fazer query para beneficios

--- src/utils/utilsTempo.ts
+++ src/utils/utilsTempo.ts
@@ -3,8 +3,6 @@ export const criarDataNoFuturo = (diasFuturo: number) => {
     const hoje = Date.now();
     const dataFuturaDias = hoje + diasFuturo * 24 * 60 * 60 * 1000;
 
-    console.log(dataFuturaDias);
-
     return dataFuturaDias;
   } catch (err: any) {
     throw new Error(err);
@@ -15,8 +13,6 @@ export const criarDataNoPassado = (diasPassado: number) => {
   try {
     const hoje = Date.now();
     const dataPassadoDias = hoje - diasPassado * 24 * 60 * 60 * 1000;
-
-    console.log(dataPassadoDias);
 
     return dataPassadoDias;
   } catch (err: any) {
