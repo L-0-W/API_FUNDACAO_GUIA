f569cd696b8e441637aa1a8e78b050e8104e0c4e
joaoluizcorreademello <joaoluiz.index@outlook.com>
2025-11-14 02:05:30 -0300

    fazendo teste de integracao para buscarVagaPorId, fazendo um each para cada id diferente

--- tests/VagasController.test.ts
+++ tests/VagasController.test.ts
@@ -5,8 +5,8 @@ const URL = "http://localhost:3003/vagas";
 describe("Testando EndPoint 'buscarVagaPorId'", () => {
   test.each([
     { id: "'", desc: "caractere inválido" },
-    { id: "1.1", desc: "decimal (não inteiro)" },
-    { id: "bac", desc: "string (não numérico)" },
+    { id: "5.0", desc: "decimal (não inteiro)" },
+    { id: "abc", desc: "string (não numérico)" },
   ])("Deve retornar 400 com parametro 'id' com %s", async ({ id, desc }) => {
     try {
       await axios.get(`${URL}/${id}`);
