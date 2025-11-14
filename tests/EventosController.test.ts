7cf3c996c8a84f071193f09666f7eec01e76c72c
camilyolivei <camilyguilhermeoliveira@gmail.com>
2025-11-14 00:25:10 -0300

    teste de EventosController feitos, foi verificado se id como flaot iria passar e se id como string iria passar

--- tests/EventosController.test.ts
+++ tests/EventosController.test.ts
@@ -5,7 +5,7 @@ import { EventosController } from "../src/controller/EventosController";
 const eventosController = new EventosController();
 
 describe("Testando EventosController", () => {
-  test("deve retornar erro com parametro 'id' errado", async () => {
+  test("deve retornar 400 com parametro 'id' errado", async () => {
     try {
       const response = await axios.get("http://localhost:3003/eventos/'");
     } catch (err) {
@@ -13,4 +13,24 @@ describe("Testando EventosController", () => {
       expect(axiosError.status).toBe(400);
     }
   });
+
+  test("deve retornar 400 com parametro 'id' que não e inteiro", async () => {
+    try {
+      const response = await axios.get("http://localhost:3003/eventos/1.1");
+    } catch (err) {
+      const axiosError = err as AxiosError;
+      expect(axiosError.status).toBe(400);
+    }
+  });
+
+  test("deve retornar 400 com parametro 'id' que e string", async () => {
+    try {
+      const response = await axios.get("http://localhost:3003/eventos/bac");
+    } catch (err) {
+      const axiosError = err as AxiosError;
+      expect(axiosError.status).toBe(400);
+    }
+  });
 });
+
+describe("Testando EventosController", () => {});
